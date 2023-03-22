69b01294b159d8549ae55c30aabf8d29fd4bbe54
conde <developer.luis.conde@gmail.com>
2023-03-22 01:45:02 -0600

    master | initial commit

---
+++ src/main/java/com/conde/mshotel/MsHotelApplication.java
@@ -0,0 +1,13 @@
+package com.conde.mshotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHotelApplication.class, args);
+	}
+
+}
