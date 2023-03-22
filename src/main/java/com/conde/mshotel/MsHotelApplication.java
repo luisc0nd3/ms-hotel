4f45a32a36344549a5ce3b94abaac57695c94f5d
conde <developer.luis.conde@gmail.com>
2023-03-22 01:53:00 -0600

    master | add project with checkstyle and sonar

--- src/main/java/com/conde/mshotel/MsHotelApplication.java
+++ src/main/java/com/conde/mshotel/MsHotelApplication.java
@@ -3,11 +3,17 @@ package com.conde.mshotel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Simple @SpringBootApplication class.
+ *
+ * @author Luis Conde
+ * @datetime 22 mar. 2023, 01:46:50
+ */
 @SpringBootApplication
 public class MsHotelApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsHotelApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MsHotelApplication.class, args);
+  }
 
 }
--- src/test/java/com/conde/mshotel/MsHotelApplicationTests.java
+++ src/test/java/com/conde/mshotel/MsHotelApplicationTests.java
@@ -1,13 +1,22 @@
 package com.conde.mshotel;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Simple @SpringBootTest class.
+ *
+ * @author Luis Conde
+ * @datetime 22 mar. 2023, 01:50:46
+ */
 @SpringBootTest
 class MsHotelApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+    assertTrue(true);
+  }
 
 }
